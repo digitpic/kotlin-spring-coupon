e42c142499911db89e2cc1a43e7f6ea7687601e4
digitpic <jkw5033@gmail.com>
2025-02-07 15:34:30 +0900

    chore: set up project

---
+++ src/main/kotlin/com/kotlin/spring/coupon/KotlinSpringCouponApplication.kt
@@ -0,0 +1,11 @@
+package com.kotlin.spring.coupon
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinSpringCouponApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinSpringCouponApplication>(*args)
+}
---
+++ src/test/kotlin/com/kotlin/spring/coupon/KotlinSpringCouponApplicationTests.kt
@@ -0,0 +1,11 @@
+package com.kotlin.spring.coupon
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinSpringCouponApplicationTests {
+    @Test
+    fun contextLoads() {
+    }
+}
