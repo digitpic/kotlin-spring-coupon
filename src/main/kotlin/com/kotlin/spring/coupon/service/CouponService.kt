ff6b386740c17458bf8d57dd3645f520a55a7418
digitpic <jkw5033@gmail.com>
2025-02-09 17:13:55 +0900

    refactor(service): 코드 정리

--- src/main/kotlin/com/kotlin/spring/coupon/service/CouponService.kt
+++ src/main/kotlin/com/kotlin/spring/coupon/service/CouponService.kt
@@ -20,25 +20,21 @@ class CouponService(
 
         val member: Member = memberRepository.findById(userId)
             .orElseThrow({ throw RuntimeException("회원 정보 없음") })
-
         if (member.isCouponIssued()) {
             return member.getCoupon()
         }
 
         val couponCount: Long = couponRepository.count()
-
         if (couponCount >= MAX_COUPON_COUNT) {
             return null
         }
 
         var coupon: Coupon = Coupon.issue()
-
         while (couponRepository.findByCode(coupon.getCode()).isNotEmpty()) {
             coupon = Coupon.issue()
         }
 
         member.issueCoupon(coupon)
-
         couponRepository.save(coupon)
 
         return coupon
